6089d17397fd593db4b33e048389f2ed5ed97ad8
al7262 <alta5@alta5>
2020-02-01 19:09:45 +0700

    basic header finished

--- src/stores/MainStore.js
+++ src/stores/MainStore.js
@@ -6,4 +6,12 @@ const initialState = {
 export const store = createStore(initialState);
 
 export const actions = (store) => ({
+    handleTogglerNavbar: () => {
+        const toggler = document.getElementById('navbarToggler')
+        if(toggler.innerHTML==='menu'){
+            toggler.innerHTML='close'
+        } else{
+            toggler.innerHTML='menu'
+        }
+    }
 });
