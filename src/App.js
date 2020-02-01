9d92f55eb7ab5fcc91d3cdd9a3eeb61608d82eef
al7262 <alta5@alta5>
2020-02-01 19:01:14 +0700

    initial project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Mainroute from './routes/MainRoute';
+
+import './styles/bootstrap.min.css'
+
+
+function App() {
+  return (
+    <Mainroute />
+  );
+}
+
+export default App;
