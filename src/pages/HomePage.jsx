75dee7761c284510ab9c71ee78b5985defd4dc1c
al7262 <alta5@alta5>
2020-02-06 11:40:26 +0700

    change homepage after login

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withRouter, Link } from "react-router-dom";
+import { withRouter, Link, Redirect } from "react-router-dom";
 import { connect } from "unistore/react";
 import { actions } from "../stores/MainStore";
 import '../styles/home.css'
@@ -7,51 +7,90 @@ import background from '../images/background2.jpg';
 import logo from  '../images/logo-light.svg';
 
 import Header from '../components/Header';
+import Loader from '../components/Loader';
 
 class HomePage extends React.Component {
     state={
-        outlet: undefined
+        finishChecking: false,
+        finishGetInfo: false,
+        isLoading: true,
     }
-    handleOnChange = (event) =>{
-        this.setState({[event.target.name]:event.target.value})
+    componentDidMount = async() =>{
+        await this.props.checkLoginStatus();
+        await this.props.handleInput('outlet', undefined)
+        this.setState({finishChecking:true})
+        if(this.props.isOwner){
+            await this.props.getOwnerInformation()
+            this.setState({finishGetInfo:true})
+            await this.props.getOutlet()
+            this.setState({isLoading:false})
+        } else{
+            await this.props.getEmployeeInformation()
+            this.setState({finishGetInfo:true})
+        }
+    }
+
+    componentWillUnmount = () => {
+        this.props.handleReset()
     }
 
     render(){
-        return (
-            <React.Fragment>
-                <Header
-                pageLocation='Beranda'/>
-                <div className="container-home">
-                    <div className="background">
-                        <img src={background} alt="background-home"/>
-                        <div className="background-mask"></div>
-                    </div>
-                    <div className="content">
-                        <h1>Selamat Datang, username</h1>
-                        <h6>Apa yang ingin anda lakukan?</h6>
-                        <form action="" className="outlet-choice" onSubmit={e=>e.preventDefault()}>
-                            <span>Outlet yang dipilih: </span>
-                            <select name="outlet" id="outlet" onChange={this.handleOnChange}>
-                                <option value={undefined} selected disabled>Pilih Outlet</option>
-                                <option value="bandung">Bandung</option>
-                            </select>
-                        </form>
-                        <div className="button-group">
-                            <Link className={"btn btn-info btn-choice " + (this.state.outlet===undefined? 'disabled': '')}>
-                                <span>Pesanan Baru</span>
-                            </Link>
-                            <Link className={"btn btn-info btn-choice " + (this.state.outlet===undefined? 'disabled': '')}>
-                                <span>Lihat Aktivitas</span>
-                            </Link>
+        if(!this.state.finishChecking){
+            return <Loader
+                height='100vh'
+                scale='3'/>
+        } if(!this.props.isLogin){
+            return <Redirect to="/login"></Redirect>
+        } else {
+            return (
+                <React.Fragment>
+                    <Header
+                    pageLocation='Beranda'/>
+                    <div className="container-home">
+                        <div className="background">
+                            <img src={background} alt="background-home"/>
+                            <div className="background-mask"></div>
+                        </div>
+                        <div className="content">
+                            <h1>Selamat Datang, {this.state.finishGetInfo? this.props.cashierName:'Loading...'}</h1>
+                            <h6>Apa yang ingin anda lakukan?</h6>
+                            {this.props.isOwner?
+                            <form action="" className="outlet-choice" onSubmit={e=>e.preventDefault()}>
+                                <span>Outlet yang dipilih: </span>
+                                <select name="outlet" id="outlet" onChange={this.props.handleOnChange}>
+                                    <option value={undefined} selected disabled>Pilih Outlet</option>
+                                    {this.state.isLoading?
+                                    <option value="" disabled>
+                                        loading....
+                                    </option>
+                                    :
+                                    Array.isArray(this.props.outletList)?
+                                    this.props.outletList.map(item=>(
+                                        <option value={item.id}>{item.name}</option>
+                                    ))
+                                    :null
+                                    }
+                                </select>
+                            </form>
+                            :null
+                            }
+                            <div className="button-group">
+                                <Link className={"btn btn-info btn-choice " + (this.props.outlet===undefined? 'disabled': '')}>
+                                    <span>Pesanan Baru</span>
+                                </Link>
+                                <Link className={"btn btn-info btn-choice " + (this.props.outlet===undefined? 'disabled': '')}>
+                                    <span>Lihat Aktivitas</span>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="logo">
+                            <img src={logo} alt="logo-application"/>
+                            <span>EasyKachin'</span>
                         </div>
                     </div>
-                    <div className="logo">
-                        <img src={logo} alt="logo-application"/>
-                        <span>EasyKachin'</span>
-                    </div>
-                </div>
-            </React.Fragment>
-        )
+                </React.Fragment>
+            )
+        }
     }
 }
-export default connect('', actions)(withRouter(HomePage));
+export default connect('isLogin, isOwner, outletList, cashierName, outlet', actions)(withRouter(HomePage));
